f3656b49ba054851fec518bea2ad9d8344e1e0b0
Thapelo-git <chabathapelo1@gmail.com>
2024-06-03 01:03:05 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import NavicationBar from './components/NavicationBar';
 import About from './components/About';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
     <Header/>
     <About/>
     <Projects/>
+    <Contact/>
     </div>
   );
 }
