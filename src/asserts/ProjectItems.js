a3d68ab70728b197f22449f0c16bc4063b9e688b
Thapelo-git <chabathapelo1@gmail.com>
2024-06-03 00:10:32 +0200

    commit

---
+++ src/asserts/ProjectItems.js
@@ -0,0 +1,25 @@
+import Project1Img from './images/hotelimg.jpg';
+
+
+
+const ProjectItems =[
+    {
+        id:1,
+        projectImg:Project1Img,
+        projectTitle:'project 1',
+        projectDescrip:'gdhfgd dvhfbvf fngvfj '
+    },
+    {
+        id:2,
+        projectImg:Project1Img,
+        projectTitle:'project 2',
+        projectDescrip:'gdhfgd dvhfbvf fngvfj '
+    },{
+        id:3,
+        projectImg:Project1Img,
+        projectTitle:'project 3',
+        projectDescrip:'gdhfgd dvhfbvf fngvfj '
+    }
+]
+
+export default ProjectItems;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import ProjectItems from '../asserts/ProjectItems'
+import ProjectCard from './ProjectCard'
 function Projects() {
   return (
     <div className='py-5' id='projects'>
         <div className='container'>
             <h2 className='mb-5'>My projects</h2>
     <div className='row g-4'>
-
+      {
+        ProjectItems.map((project)=> <ProjectCard key=
+        {project.id} projectImage={project.projectImg}
+        projectName={project.projectTitle} projectDescrip={project.projectDescrip}/>)
+      }
     </div>
         </div>
     </div>
